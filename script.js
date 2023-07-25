70bf2a95b28f7d00d2936c473e7bd39170375550
Leonardo Rodrigues Martins <117056433+Leonardordgm@users.noreply.github.com>
2023-07-25 11:47:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleNav() {
+    var nav = document.querySelector('.navbar');
+    var button = document.querySelector('.button');
+
+    nav.classList.toggle("open");
+    button.classList.toggle("open");
+}
